75d5c4e01c411fceb371027b3b128a4388251ef4
minghl <a879307253@gmail.com>
2023-04-11 18:00:07 +0800

    Ng13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   // 数据绑定
   message:string = "Hello Angular";
   htmlString: string = "<h1>htmlString</h1>";
@@ -14,5 +14,34 @@ export class AppComponent {
   };
   // 属性绑定
   title = "angular-test";
-  imgUrl = "https://img0.baidu.com/it/u=3006243109,1966449273&fm=253&fmt=auto&app=120&f=JPEG?w=569&h=273"
+  imgUrl = "https://img0.baidu.com/it/u=3006243109,1966449273&fm=253&fmt=auto&app=120&f=JPEG?w=569&h=273";
+  // 事件绑定
+  // onClick(event: Event){
+  //   console.log(event);
+    
+  //   alert("按钮被点击了");
+  //   console.log(this);
+  // }
+  onKeyup(){
+    console.log("onKeyup");
+  }
+
+  // 使用 ViewChild 装饰器获取一个元素
+  @ViewChild("paragraph") paragraph: ElementRef<HTMLParagraphElement> | undefined;
+
+  // 使用 ViewChildren 获取一组元素
+  @ViewChildren("items") items: QueryList<ElementRef<HTMLLIElement>> | undefined;
+
+  // 获取原生DOM对象，在组件模版中
+  onClick(button:HTMLButtonElement):void{
+    console.log(button);
+  }
+
+  // 获取原生DOM对象，在组件类中
+  ngAfterViewInit(): void {
+    // ?.安全修饰符，paragraph有的时候才去访问nativeElement
+    console.log(this.paragraph?.nativeElement);
+    console.log(this.items?.toArray());
+    
+  }
 }
