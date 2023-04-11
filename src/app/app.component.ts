dce9de680714d1c67f141ec8b717daea522aabb7
minghl <a879307253@gmail.com>
2023-04-11 17:16:34 +0800

    Ng01-10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'angular-test';
+  // 数据绑定
+  message:string = "Hello Angular";
+  htmlString: string = "<h1>htmlString</h1>";
+  getInfo(){
+    return "我是getInfo方法中返回的内容"
+  };
+  // 属性绑定
+  title = "angular-test";
+  imgUrl = "https://img0.baidu.com/it/u=3006243109,1966449273&fm=253&fmt=auto&app=120&f=JPEG?w=569&h=273"
 }
